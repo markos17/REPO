37a8322679bbf38a27ee992732d35321621615fb
szym_lenovo <szym17@gmail.com>
2016-05-05 22:00:34 +0200

    zmiana etykiety

--- Projekt1/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ Projekt1/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            label1.Text = "clicked";
+            label1.Text = "clicked 2ga ver";
         }
     }
 }
