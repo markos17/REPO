dc0e824fee1443ebca1ccbe20e41ba16e5334c47
Dell <szym17@gmail.com>
2017-03-29 00:43:07 +0200

    cookBook ch1

---
+++ cookbook/CookBook/CookBook/Ch1_10_FixedSizeCollection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBook
+{
+    public class Ch1_10_FixedSizeCollection
+    {
+        public static int InstanceCount { get; set; }
+        public int ItemCount { get; private set; }
+        private object[] Items { get; set; }
+        public override string ToString() =>
+            $"There are {Ch1_10_FixedSizeCollection.InstanceCount.ToString()} instances of {this.GetType().ToString()} and this instance contains {this.ItemCount} items...";
+
+        public Ch1_10_FixedSizeCollection(int maxItems)
+        {
+            Ch1_10_FixedSizeCollection.InstanceCount++;
+            this.Items = new object[maxItems];
+        }
+
+        public int AddItem(object item)
+        {
+            if (ItemCount < Items.Length)
+            {
+                Items[ItemCount] = item;
+                return ItemCount++;
+            }
+            throw new Exception("Exceed maximum items in collection.");
+        }
+
+        public object GetItem(int index)
+        {
+            if (index >= Items.Length && index >= 0)
+                throw new ArgumentOutOfRangeException(nameof(index));
+            return Items[index];
+        }
+    }
+}
