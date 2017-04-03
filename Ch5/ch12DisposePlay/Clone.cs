cf2cdb752a621773b5180cde9e4e29ccdffee04e
Dell <szym17@gmail.com>
2017-04-04 01:02:05 +0200

    ch12 finalizer

--- Ch5/ch12DisposePlay/Clone.cs
+++ Ch5/ch12DisposePlay/Clone.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace ch12DisposePlay
 {
-
+    [Serializable]
     class Clone : IDisposable
     {
         public int Id { get; private set; }
+
 
         public Clone(int Id)
         {
@@ -19,8 +22,18 @@ namespace ch12DisposePlay
 
         public void Dispose()
         {
-            MessageBox.Show("I’ve been disposed!",
-                "Clone #" + Id + " says...");
+            //  MessageBox.Show("I’ve been disposed!" + "Clone #" + Id + " says...");
+            string filename = @"C:\Temp\Clone.dat";
+            string dirname = @"C:\Temp\";
+            if (File.Exists(filename) == false)
+            {
+                Directory.CreateDirectory(dirname);
+            }
+            BinaryFormatter bf = new BinaryFormatter();
+            using (Stream output = File.OpenWrite(filename))
+            {
+                bf.Serialize(output, this);
+            }
         }
 
         ~Clone()
