7d2e511e065e40906f7d9be290de5714c715c933
Dell <szym17@gmail.com>
2017-04-09 20:55:51 +0200

    ch13

---
+++ Ch5/ch13PoolPuzzle/PoolPuzzle/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch13PoolPuzzle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Faucet();
+            Console.ReadKey();
+        }
+    }
+}
